21e648a2f8902c1833a95380ab599323f60036f0
Alex Cuji Torres <alexcristopher96@gmail.com>
2022-09-25 21:07:35 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MiPrimeraApp';
+  mensaje="";
+  registro = false;
+  nombre:String="";
+  apellido = "";
+
+  registrarUsuarios():void{
+    if(this.nombre=="" || this.apellido==""){
+      alert("Llene todos los campos");
+    }else{
+    this.registro = true;
+    this.mensaje ="Registrado Correctamente";
+  }
+}
 }
